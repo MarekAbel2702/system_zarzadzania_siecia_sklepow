64331b4010f880211f5c97d1d3df8472248d8306
MarekAbel2702 <marek-abel@wp.pl>
2025-07-28 23:18:47 +0200

    Dodanie funkcji: top5_produkty_miesiac, top_pracownicy_miesiac

--- sql/05_functions.sql
+++ sql/05_functions.sql
@@ -19,3 +19,35 @@ END
 
 SELECT dbo.oblicz_marze(1) AS mar¿a_procent;
 
+CREATE OR ALTER FUNCTION top5_produkty_miesiac (@rok INT, @miesiac INT)
+RETURNS TABLE
+AS
+RETURN
+	SELECT TOP 5 p.id_produktu, p.nazwa, SUM(ps.ilosc) AS ilosc_sprzedana FROM produkty p
+	JOIN pozycjeSprzedazy ps
+	ON p.id_produktu = ps.id_produktu
+	JOIN sprzedaz s
+	ON ps.id_sprzedazy = s.id_sprzedazy
+	WHERE @rok = YEAR(s.data) AND @miesiac = MONTH(s.data)
+	GROUP BY p.id_produktu, p.nazwa
+	ORDER BY SUM(ps.ilosc) DESC;
+
+SELECT * FROM dbo.top5_produkty_miesiac(2025, 7);
+
+CREATE OR ALTER FUNCTION top_pracownicy_miesiac (@rok INT, @miesiac INT)
+RETURNS TABLE 
+AS
+RETURN 
+SELECT 
+	p.id_pracownika, 
+	p.imie + ' ' +  p.nazwisko AS imie_nazwisko, 
+	COUNT(s.id_sprzedazy) AS liczba_transakcji, 
+	SUM(s.suma) AS suma_sprzedazy 
+FROM pracownicy p
+JOIN sprzedaz s
+ON p.id_pracownika = s.id_pracownika
+WHERE @rok = YEAR(s.data) AND @miesiac = MONTH(s.data)
+GROUP BY p.id_pracownika, p.imie, p.nazwisko;
+
+SELECT * FROM dbo.top_pracownicy_miesiac(2025, 7)
+ORDER BY suma_sprzedazy DESC;
